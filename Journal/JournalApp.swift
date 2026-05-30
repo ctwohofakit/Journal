f9c0fa1ed03e2d73a424980299907b08e84b871b
ctwohofakit <ctwohofa@gmail.com>
2026-05-30 12:27:28 -0700

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Kit Sito on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
